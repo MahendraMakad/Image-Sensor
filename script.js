662da05a3d6ef32bb94d0314cbf994dab0d941e5
MahendraMakad <87107054+MahendraMakad@users.noreply.github.com>
2023-03-16 16:53:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 // Select the input file element
 const fileInput = document.querySelector('#file-input');
+// global variable to preserve original image data
 var originalData;
-var x1,y1,x2,y2;
+// global varibles for selected coordinates of image using imgareaselect
+var x1, y1, x2, y2;
+// checkbox for auto preview
+var checkbox = document.getElementById("auto-preview-btn");
+// selecting the image canvas
 var canvas = document.getElementById('image-canvas');
 var ctx = canvas.getContext("2d");
 
+// calling onclick function for button pixelate when auto-preview is 
+// not enabled
+$('#pixelate-btn').click(function () {
+    pixelate(x1, y1, x2, y2);
+});
 
+// onchage event on loading of a file which will draw image and canvas for
+// original image
 $('#file-input').change(function () {
     var file = this.files[0];
     var image = new Image();
@@ -17,9 +29,9 @@ $('#file-input').change(function () {
         image.onload = function () {
             var canvasWidth = image.width;
             var canvasHeight = image.height;
-            if (canvasWidth > 300) {
-                canvasHeight = canvasHeight * (300 / canvasWidth);
-                canvasWidth = 300;
+            if (canvasWidth > 400) {
+                canvasHeight = canvasHeight * (400 / canvasWidth);
+                canvasWidth = 400;
             }
             canvas.width = canvasWidth;
             canvas.height = canvasHeight;
@@ -45,24 +57,7 @@ $('#file-input').change(function () {
                                 h = selection.height;
                                 console.log(x1, y1, x2, y2);
                                 // perform further processing
-                                pixelate();
-                            }
-                        }, 1000);
-                    },
-                    onSelectChange: function(img,selection) {
-                        selectionArea = selection;
-                        setTimeout(function () {
-                            // use selectedArea after a delay of 1 second
-                            if (selectedArea !== null) {
-                                x1 = selectedArea.x1;
-                                y1 = selectedArea.y1;
-                                x2 = selectedArea.x2;
-                                y2 = selectedArea.y2;
-                                w = selection.width;
-                                h = selection.height;
-                                console.log(x1, y1, x2, y2);
-                                // perform further processing
-                                pixelate();
+                                if (checkbox.checked) pixelate();
                             }
                         }, 1000);
                     }
@@ -71,9 +66,6 @@ $('#file-input').change(function () {
             if (canvas.width && canvas.height) {
                 console.log("canvas is complete");
                 initImgAreaSelect();
-            } else {
-                console.log("canvas is not complete");
-                canvas.addEventListener('load', initImgAreaSelect);
             }
         };
     };
@@ -135,7 +127,48 @@ function pixelate() {
     // Update the canvas with the modified image data.
     ctx.putImageData(imageData, 0, 0);
     console.log("pixelated");
-    //   }
-    //  });
-
 }
+
+//add eventListner to JPG download button
+// add event listener to button
+document.getElementById("imageJPG").addEventListener("click", function () {
+
+    if (canvas.height && canvas.width) {
+        // get the canvas data as a JPG image
+        let imageData = canvas.toDataURL("image/jpeg");
+
+        // create a link element and set its download attribute
+        const link = document.createElement("a");
+        link.download = "img.jpg";
+
+        // set the href attribute to the canvas data
+        link.href = imageData;
+
+        // add the link to the document and simulate a click
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+});
+
+
+//add eventListner to PNG download button
+// add event listener to button
+document.getElementById("imagePNG").addEventListener("click", function () {
+    // get the canvas data as a JPG image
+    if (canvas.height && canvas.width) {
+        let imageData = canvas.toDataURL("image/png");
+
+        // create a link element and set its download attribute
+        const link = document.createElement("a");
+        link.download = "img.png";
+
+        // set the href attribute to the canvas data
+        link.href = imageData;
+
+        // add the link to the document and simulate a click
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+});
